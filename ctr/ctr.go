0916a3dbdd8d02bccd49522c7d04ddcac6edad75
Gaspar Almada <a.gaspar.it@gmail.com>
2021-12-01 23:21:11 -0300

    add: now it's possible to play against the computer

--- ctr/ctr.go
+++ ctr/ctr.go
@@ -2,5 +2,236 @@ package ctr
 
 // Este package contiene los controladores del juego.
 
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+)
+
+var BoardGame = []string{
+	"  |   |  ",
+	"──┼───┼──",
+	"  |   |  ",
+	"──┼───┼──",
+	"  |   |  ",
+}
+
+// var places = map[int]string{
+// 	1: "00",
+// 	2: "04",
+// 	3: "08",
+// 	4: "20",
+// 	5: "24",
+// 	6: "28",
+// 	7: "40",
+// 	8: "42",
+// 	9: "48",
+// }
+
+const MARGIN = "            "
+const PLAYER = "O"
+const COMPUTER = "X"
+
+var Cursor = "*"
+var CursorInv = string(BoardGame[CursorY][CursorX]) // Guardo el character que se debe mostrar cuando el cursor se hace invisible al parpadear.
+
 var CursorY = 2
 var CursorX = 4
+
+const TURN_PLAYER = "Turn: Player"
+const TURN_COMPUTER = "Turn: Computer"
+
+var Message = TURN_PLAYER
+var lastCompY = ""
+var lastCompX = ""
+
+const BLINK_TIME = 300
+
+func PlayerWinControl() { // Controlo si el Jugador o la Computadora (PLAYER = PLAYER o COMPUTER) ganó tras su ultima jugada.
+
+	// Si en la FILA en la que voy a poner una pieza ya hay dos piezas puesas, se gana la partida.
+	if strings.Count(BoardGame[CursorY], PLAYER) == 2 {
+		showWinner(PLAYER)
+		return
+	}
+
+	//  Si en la COLUMNA en la que voy a poner una pieza ya hay dos piezas puestas, se gana la partida.
+	count := 0
+	for y := 0; y <= len(BoardGame); y = y + 2 {
+		if string(BoardGame[y][CursorX]) == PLAYER {
+			count++
+		}
+	}
+	if count == 2 {
+		showWinner(PLAYER)
+		return
+	}
+
+	// Si en la DIAGONAL (\) en la que voy a poner una pieza ya hay dos piezas puesas, se gana la partida.
+	if ((CursorY == 0 && CursorX == 0) && string(BoardGame[2][4]) == PLAYER && string(BoardGame[4][8]) == PLAYER) ||
+		((CursorY == 2 && CursorX == 4) && string(BoardGame[0][0]) == PLAYER && string(BoardGame[4][8]) == PLAYER) ||
+		((CursorY == 4 && CursorX == 8) && string(BoardGame[0][0]) == PLAYER && string(BoardGame[2][4]) == PLAYER) {
+		showWinner(PLAYER)
+		return
+	}
+
+	// Si en la DIAGONAL (/) en la que voy a poner una pieza ya hay dos piezas puesas, se gana la partida.
+	if ((CursorY == 0 && CursorX == 8) && string(BoardGame[2][4]) == PLAYER && string(BoardGame[4][0]) == PLAYER) ||
+		((CursorY == 2 && CursorX == 4) && string(BoardGame[0][8]) == PLAYER && string(BoardGame[4][0]) == PLAYER) ||
+		((CursorY == 4 && CursorX == 0) && string(BoardGame[0][8]) == PLAYER && string(BoardGame[2][4]) == PLAYER) {
+		showWinner(PLAYER)
+		return
+	}
+
+	changeTurn()
+}
+
+func changeTurn() { // Cambio el mensaje de a quien le toca jugar por no haberse jenerado un ganador en el anterior turno
+	if Message == TURN_PLAYER {
+		Message = TURN_COMPUTER
+		computerElection()
+	} else {
+		Message = TURN_PLAYER
+	}
+}
+
+func DeleteCrusor() { // Borro el cursor en su posición actual por estar por moverse a una nueva posición.
+	BoardGame[CursorY] = fmt.Sprintf("%s%s%s", BoardGame[CursorY][:CursorX], CursorInv, BoardGame[CursorY][CursorX+1:])
+}
+
+func Render() {
+	rand.Seed(time.Now().UnixNano()) // Preparo la variable rand para que pueda generar numeros randoms.
+
+	for {
+
+		if Message == TURN_PLAYER { // El cursor solo debe parpadear si es el turno del jugador
+			RenderBoardgame()
+			time.Sleep(time.Millisecond * BLINK_TIME)                                                                        // Genero una demora para que se vea el cursor
+			BoardGame[CursorY] = fmt.Sprintf("%s%s%s", BoardGame[CursorY][:CursorX], Cursor, BoardGame[CursorY][CursorX+1:]) // Actualizo la posicion del cursor segun su ultima posición definida por el jugador.
+			ClearScreen()
+		}
+
+		RenderBoardgame()
+		time.Sleep(time.Millisecond * BLINK_TIME)                                                                           // Genero una demora para que se vea lo que esta debajo del cursor
+		BoardGame[CursorY] = fmt.Sprintf("%s%s%s", BoardGame[CursorY][:CursorX], CursorInv, BoardGame[CursorY][CursorX+1:]) // Actualizo la posicion del cursor segun su ultima posición definida por el jugador.
+		ClearScreen()
+
+	}
+}
+
+func RenderBoardgame() { // Renderizo el tablero de juego junto con información del juego.
+	for i := 0; i < len(BoardGame); i++ {
+		fmt.Print(MARGIN)
+		fmt.Println(BoardGame[i])
+	}
+	fmt.Println(Message, lastCompY, lastCompX)
+}
+
+func ClearScreen() {
+	if strings.Contains(runtime.GOOS, "windows") { // windows
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		cmd := exec.Command("clear") // linux or mac
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func HideTerminalCursor(option string) { // Oculto el cursor de la terminal
+	cmd := exec.Command("tput", option)
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func showWinner(winner string) { // Muestro un mensaje indicando quen gano la partida.
+	if winner == PLAYER {
+		Message = "Player Wins!!"
+	} else {
+		Message = "Computer Wins!!"
+	}
+}
+
+func computerElection() {
+	time.Sleep(time.Millisecond * BLINK_TIME * 5) // Genero una demora para simular el pensar de la computadora
+
+	var y int
+	var x int
+
+	for {
+
+		var randNumY = rand.Intn(3)
+		var randNumX = rand.Intn(3)
+
+		if randNumY == 0 {
+			y = 0
+		} else if randNumY == 1 {
+			y = 2
+		} else if randNumY == 2 {
+			y = 4
+		}
+
+		if randNumX == 0 {
+			x = 0
+		} else if randNumX == 1 {
+			x = 4
+		} else if randNumX == 2 {
+			x = 8
+		}
+
+		// ENTRA A ESTE IF SIN QUE DEBA HACERLO
+		if string(BoardGame[y][x]) != string(PLAYER) &&
+			string(BoardGame[y][x]) != string(COMPUTER) &&
+			string(BoardGame[y][x]) != string(Cursor) { // Poner != " " lleva que la computadora pueda elegir el ultimo lugar que eligio el jugador.
+			// lastCompX = strconv.Itoa(x)
+			// lastCompY = strconv.Itoa(y)
+			BoardGame[y] = fmt.Sprintf("%s%s%s", BoardGame[y][:x], COMPUTER, BoardGame[y][x+1:])
+			ClearScreen()
+			RenderBoardgame()
+			computerWinControl(y, x)
+			break
+		}
+
+	}
+}
+
+func computerWinControl(y, x int) {
+	if strings.Count(BoardGame[y], COMPUTER) == 3 {
+		showWinner(COMPUTER)
+		return
+	}
+
+	count := 0
+	for i := 0; i <= len(BoardGame); i = i + 2 {
+		if string(BoardGame[i][x]) == COMPUTER {
+			count++
+		}
+	}
+	if count == 3 {
+		showWinner(COMPUTER)
+		return
+	}
+
+	if (string(BoardGame[0][0]) == COMPUTER && string(BoardGame[2][4]) == COMPUTER && string(BoardGame[4][8]) == COMPUTER) || // Diagonal \
+		(string(BoardGame[0][8]) == COMPUTER && string(BoardGame[2][4]) == COMPUTER && string(BoardGame[4][0]) == COMPUTER) { // Diagonal /
+		showWinner(COMPUTER)
+		return
+	}
+
+	changeTurn()
+	// 1º columna OK
+	// 2º columna OK 3
+	// 3º columna OK 3
+
+	// 1º fila    OK 2
+	// 2º fila    OK 3
+	// 3º fila    OK 2
+
+	// Diagonal /  OK
+	// Diagonal \  OK
+}
--- main.go
+++ main.go
@@ -4,35 +4,25 @@ import (
 	"fmt"
 	"log"
 	"myapp/ctr"
-	"myapp/gra"
-	"os"
-	"os/exec"
-	"runtime"
-	"strings"
-	"time"
 
 	"github.com/eiannone/keyboard"
 )
 
-const BLINK_TIME = 300
-
-var winnerIs = ""
-
 func main() {
 
-	err := keyboard.Open()
+	err := keyboard.Open() // Activo la funcionalidad que lee las teclas presionadas.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer func() {
-		hideTerminalCursor("cvvis")
+	defer func() { // Me aseguro que se desactive la funcionalidad que lee las teclas presionadas y que se reactive el cursor de la terminal al finalizar esta aplicación.
+		ctr.HideTerminalCursor("cvvis")
 		_ = keyboard.Close()
 	}()
 
-	clearScreen()
-	hideTerminalCursor("civis")
-	go render()
+	ctr.ClearScreen()               // Limpio la terminal
+	ctr.HideTerminalCursor("civis") // Desactivo el cursor de la terminal
+	go ctr.Render()                 // Ejecuto el proceso de renderizado del jugo como una co-routina
 
 	for {
 		// Registramos que tecla presiono el usuario.
@@ -41,142 +31,45 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// Detectamos si el usuario presiono la tecla para salir del proigrama:
+		// Detectamos si el usuario presiono la tecla para salir del programa:
 		if key == keyboard.KeyEsc {
 			break
 		}
 
-		switch key {
-		case keyboard.KeyArrowRight:
-			if ctr.CursorX < 8 { // Impido que el cursor se salga del tablero.
-				deleteCrusor()                                                    // Borro el cursor en su posición actual por moverse a una nueva posición.
-				gra.CursorInv = string(gra.Boardgame[ctr.CursorY][ctr.CursorX+4]) // Almaceno el character que ya esta en el lugar adonde se va a colocar el cursor. Lo convertirlo a string porque es un byte.
-				ctr.CursorX = ctr.CursorX + 4                                     // Actualizo la posición del cursor segun la tecla presionada.
+		if ctr.Message == ctr.TURN_PLAYER {
+			switch key { // Realizo una acción segun la tecla presionada por el jugador.
+			case keyboard.KeyArrowRight:
+				if ctr.CursorX < 8 { // Impido que el cursor se salga del tablero.
+					ctr.DeleteCrusor()                                                // Borro el cursor en su posición actual por moverse a una nueva posición.
+					ctr.CursorInv = string(ctr.BoardGame[ctr.CursorY][ctr.CursorX+4]) // Almaceno el character que ya esta en el lugar adonde se va a colocar el cursor. Lo convertirlo a string porque es un byte.
+					ctr.CursorX = ctr.CursorX + 4                                     // Actualizo la posición del cursor segun la tecla presionada.
+				}
+			case keyboard.KeyArrowLeft:
+				if ctr.CursorX > 0 {
+					ctr.DeleteCrusor()
+					ctr.CursorInv = string(ctr.BoardGame[ctr.CursorY][ctr.CursorX-4])
+					ctr.CursorX = ctr.CursorX - 4
+				}
+			case keyboard.KeyArrowDown:
+				if ctr.CursorY < 4 {
+					ctr.DeleteCrusor()
+					ctr.CursorInv = string(ctr.BoardGame[ctr.CursorY+2][ctr.CursorX])
+					ctr.CursorY = ctr.CursorY + 2
+				}
+			case keyboard.KeyArrowUp:
+				if ctr.CursorY > 0 {
+					ctr.DeleteCrusor()
+					ctr.CursorInv = string(ctr.BoardGame[ctr.CursorY-2][ctr.CursorX])
+					ctr.CursorY = ctr.CursorY - 2
+				}
+			case keyboard.KeySpace: // Si el jugador quiere poner su pieza y si esta vacio el lugar que ocupa el cursor, la pieza del jugador es colocada.
+				if ctr.CursorInv == " " {
+					ctr.PlayerWinControl()
+					ctr.CursorInv = ctr.PLAYER
+				}
+			default:
+				fmt.Print(string(char))
 			}
-		case keyboard.KeyArrowLeft:
-			if ctr.CursorX > 0 {
-				deleteCrusor()
-				gra.CursorInv = string(gra.Boardgame[ctr.CursorY][ctr.CursorX-4])
-				ctr.CursorX = ctr.CursorX - 4
-			}
-		case keyboard.KeyArrowDown:
-			if ctr.CursorY < 4 {
-				deleteCrusor()
-				gra.CursorInv = string(gra.Boardgame[ctr.CursorY+2][ctr.CursorX])
-				ctr.CursorY = ctr.CursorY + 2
-			}
-		case keyboard.KeyArrowUp:
-			if ctr.CursorY > 0 {
-				deleteCrusor()
-				gra.CursorInv = string(gra.Boardgame[ctr.CursorY-2][ctr.CursorX])
-				ctr.CursorY = ctr.CursorY - 2
-			}
-		case keyboard.KeySpace:
-			if gra.CursorInv == " " { // Si esta vacio el lugar que ocupa el cursor, poner la pieza del jugador.
-				winControl()
-				gra.CursorInv = gra.PLAYER
-			}
-		case keyboard.KeyEnter:
-			if gra.CursorInv == " " {
-				gra.CursorInv = gra.COMPUTER
-			}
-		default:
-			fmt.Print(string(char))
 		}
 	}
-}
-
-func winControl() {
-	// Control de filas ─
-	// Si en la fila en la que voy a poner una pieza ya hay dos piezas puesas, se gana la partida.
-	if strings.Count(gra.Boardgame[ctr.CursorY], gra.PLAYER) == 2 {
-		winnerIs = "Player Wins!!"
-		return
-	}
-
-	// Control de columnas |
-	//  Si en la columna en la que voy a poner una pieza ya hay dos piezas puestas, se gana la partida.
-	count := 0
-	for y := 0; y <= len(gra.Boardgame); y = y + 2 {
-		if string(gra.Boardgame[y][ctr.CursorX]) == gra.PLAYER {
-			count++
-		}
-	}
-	if count == 2 {
-		winnerIs = "Player Wins!!"
-		return
-	}
-
-	// Control de diagonal \
-	// Si en la diagonal en la que voy a poner una pieza ya hay dos piezas puesas, se gana la partida.
-	if (ctr.CursorY == 0 && ctr.CursorX == 0) && string(gra.Boardgame[2][4]) == gra.PLAYER && string(gra.Boardgame[4][8]) == gra.PLAYER {
-		winnerIs = "Player Wins!!"
-		return
-	} else if (ctr.CursorY == 2 && ctr.CursorX == 4) && string(gra.Boardgame[0][0]) == gra.PLAYER && string(gra.Boardgame[4][8]) == gra.PLAYER {
-		winnerIs = "Player Wins!!"
-		return
-	} else if (ctr.CursorY == 4 && ctr.CursorX == 8) && string(gra.Boardgame[0][0]) == gra.PLAYER && string(gra.Boardgame[2][4]) == gra.PLAYER {
-		winnerIs = "Player Wins!!"
-		return
-	}
-
-	//Control de diagonal /
-	// Si en la diagonal en la que voy a poner una pieza ya hay dos piezas puesas, se gana la partida.
-	if (ctr.CursorY == 0 && ctr.CursorX == 8) && string(gra.Boardgame[2][4]) == gra.PLAYER && string(gra.Boardgame[4][0]) == gra.PLAYER {
-		winnerIs = "Player Wins!!"
-		return
-	} else if (ctr.CursorY == 2 && ctr.CursorX == 4) && string(gra.Boardgame[0][8]) == gra.PLAYER && string(gra.Boardgame[4][0]) == gra.PLAYER {
-		winnerIs = "Player Wins!!"
-		return
-	} else if (ctr.CursorY == 4 && ctr.CursorX == 0) && string(gra.Boardgame[0][8]) == gra.PLAYER && string(gra.Boardgame[2][4]) == gra.PLAYER {
-		winnerIs = "Player Wins!!"
-		return
-	}
-
-}
-
-func deleteCrusor() { // Borro el cursor en su posición actual por moverse a una nueva posición.
-	gra.Boardgame[ctr.CursorY] = fmt.Sprintf("%v%v%v", gra.Boardgame[ctr.CursorY][:ctr.CursorX], gra.CursorInv, gra.Boardgame[ctr.CursorY][ctr.CursorX+1:])
-	clearScreen()
-	renderBoardgame()
-}
-
-func render() {
-	for {
-		renderBoardgame()
-		time.Sleep(time.Millisecond * BLINK_TIME)
-		gra.Boardgame[ctr.CursorY] = fmt.Sprintf("%v%v%v", gra.Boardgame[ctr.CursorY][:ctr.CursorX], gra.Cursor, gra.Boardgame[ctr.CursorY][ctr.CursorX+1:])
-		clearScreen()
-
-		renderBoardgame()
-		time.Sleep(time.Millisecond * BLINK_TIME)
-		gra.Boardgame[ctr.CursorY] = fmt.Sprintf("%v%v%v", gra.Boardgame[ctr.CursorY][:ctr.CursorX], gra.CursorInv, gra.Boardgame[ctr.CursorY][ctr.CursorX+1:])
-		clearScreen()
-	}
-}
-
-func renderBoardgame() {
-	for i := 0; i < len(gra.Boardgame); i++ {
-		fmt.Print(gra.MARGIN)
-		fmt.Println(gra.Boardgame[i])
-	}
-	fmt.Println(winnerIs)
-}
-
-func clearScreen() {
-	if strings.Contains(runtime.GOOS, "windows") { // windows
-		cmd := exec.Command("cmd", "/c", "cls")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	} else {
-		cmd := exec.Command("clear") // linux or mac
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-}
-
-func hideTerminalCursor(option string) {
-	cmd := exec.Command("tput", option)
-	cmd.Stdout = os.Stdout
-	cmd.Run()
 }
